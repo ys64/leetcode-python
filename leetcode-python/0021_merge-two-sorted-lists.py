bac0baf4d094db3fba0f585eed4100474133cc90
ys64 <yasuwagon@yahoo.co.jp>
2021-04-13 18:15:22 -0600

    Add Solution2 to 0021

--- leetcode-python/0021_merge-two-sorted-lists.py
+++ leetcode-python/0021_merge-two-sorted-lists.py
@@ -56,6 +56,34 @@ class Solution:
         return temp
 
 
+class Solution2:
+    # 04/13/2021
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        placeholder = temp = ListNode()
+
+        while l1 is not None or l2 is not None:
+            if l1 is None:
+                temp.next = l2
+                break
+
+            if l2 is None:
+                temp.next = l1
+                break
+
+            if l1.val <= l2.val:
+                temp.next = ListNode(l1.val)
+                temp = temp.next
+                l1 = l1.next
+            elif l2.val < l1.val:
+                temp.next = ListNode(l2.val)
+                temp = temp.next
+                l2 = l2.next
+            else:
+                print("error")
+
+        return placeholder.next
+
+
 s = Solution()
 print(
     s.mergeTwoLists(
