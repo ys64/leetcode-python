f3d89f484f8a5bd6d841b8e48466d1e1ec23062f
ys64 <yasuwagon@yahoo.co.jp>
2021-04-14 20:47:09 -0600

    Add 0027

---
+++ leetcode-python/0027_remove-element.py
@@ -0,0 +1,30 @@
+from typing import List
+
+
+class Solution:
+    # 07/15/2020
+    def removeElement(self, nums: List[int], val: int) -> int:
+        ### This works, but elements which are equal to val
+        ### need to be removed from nums.
+        # count = 0
+
+        # for n in nums:
+        #     if n == val:
+        #         count += 1
+
+        # return len(nums) - count
+
+        i = 0
+        while i < len(nums):
+            if nums[i] == val:
+                nums.remove(val)
+            else:
+                i += 1
+
+        print(nums)
+        return len(nums)
+
+
+s = Solution()
+print(s.removeElement([3, 2, 2, 3], 3))
+print(s.removeElement([0, 1, 2, 2, 3, 0, 4, 2], 2))
