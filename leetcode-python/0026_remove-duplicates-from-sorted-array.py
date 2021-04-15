9c84f65079a142088776300c76f06539e0697a35
ys64 <yasuwagon@yahoo.co.jp>
2021-04-14 20:33:49 -0600

    Add Solution2 to 0026

--- leetcode-python/0026_remove-duplicates-from-sorted-array.py
+++ leetcode-python/0026_remove-duplicates-from-sorted-array.py
@@ -20,6 +20,23 @@ class Solution:
         return len(nums)
 
 
+class Solution2:
+    # 04/14/2021
+    def removeDuplicates(self, nums: List[int]) -> int:
+        # num is equal to or smaller than -10^4
+        current_num = -(10 ** 5)
+        current_index = 0
+
+        while len(nums) != current_index:
+            if nums[current_index] == current_num:
+                nums.remove(current_num)
+            else:
+                current_num = nums[current_index]
+                current_index += 1
+
+        return len(nums)
+
+
 s = Solution()
 print(s.removeDuplicates([1, 2]))
 print(s.removeDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]))
