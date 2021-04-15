167cd20879dbf26f3d87ace024f81cfd080b6d75
ys64 <yasuwagon@yahoo.co.jp>
2021-04-14 21:48:39 -0600

    Add 0035

---
+++ leetcode-python/0035_search-insert-position.py
@@ -0,0 +1,26 @@
+from typing import List
+
+
+class Solution:
+    # 07/20/2020
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        ans = 0
+
+        for i, n in enumerate(nums):
+            if n == target:
+                return i
+            else:
+                if n < target:
+                    ans = i
+                else:
+                    return i
+
+        return len(nums)
+
+
+s = Solution()
+print(s.searchInsert([1, 3, 5, 6], 5))
+print(s.searchInsert([1, 3, 5, 6], 2))
+print(s.searchInsert([1, 3, 5, 6], 7))
+print(s.searchInsert([1, 3, 5, 6], 0))
+print(s.searchInsert([1], 0))
