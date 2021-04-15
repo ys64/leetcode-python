22187eb0a013aa1cc652f9f5f278009e25bfac52
ys64 <yasuwagon@yahoo.co.jp>
2021-04-14 20:47:30 -0600

    Add Solution2 to 0027

--- leetcode-python/0027_remove-element.py
+++ leetcode-python/0027_remove-element.py
@@ -25,6 +25,19 @@ class Solution:
         return len(nums)
 
 
+class Solution2:
+    # 04/14/2021
+    def removeElement(self, nums: List[int], val: int) -> int:
+        i = 0
+        while i != len(nums):
+            if nums[i] == val:
+                nums.remove(val)
+            else:
+                i += 1
+
+        return len(nums)
+
+
 s = Solution()
 print(s.removeElement([3, 2, 2, 3], 3))
 print(s.removeElement([0, 1, 2, 2, 3, 0, 4, 2], 2))
